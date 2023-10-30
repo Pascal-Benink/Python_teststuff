22549af45878c5707a41b48dc4a09aa8cc388ec9
Pascal-Benink <112872169+Pascal-Benink@users.noreply.github.com>
2023-10-30 13:54:18 +0100

    a s ton added

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,20 @@
+import os
+import sys
+import subprocess
+def restart_program():
+    python = sys.executable
+    os.execl(python, python, *sys.argv)
+
+
 n1 = input("Enter a number: ")
-op = input("Enter a opperator (e.g. *, /, +, -): ")
+while n1 == "":
+    n1 = input("Enter a number: ")
+op = input("Enter a operator (e.g. *, /, +, -): ")
+while op == "":
+    op = input("Enter a operator (e.g. *, /, +, -): ")
 n2 = input("Enter another number: ")
+while n2 == "":
+    n2 = input("Enter a number: ")
 
 if n1 == "pie":
     n1=3.14
@@ -19,5 +33,20 @@ elif op == "/":
     result = num1 / num2
 elif op == "*":
     result = num1 * num2
+else:
+    print("you did not put the expected operator the Calculator will now be restarted")
+    restart_program()
 
 print("Result: " + str(result))
+
+acceptance = input("Do you accept this answer Y/N: ")
+
+if acceptance == "Y":
+    restart = input("Do you want to restart the script? Y/N: ")
+    if restart == "Y":
+        restart_program()
+elif acceptance == "N":
+    print('Script will now exit')
+else:
+    print("you did not put the expected answer the Calculator will now be restarted")
+    restart_program()
